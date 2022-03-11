7c18020b172d4b09c6230f6c8ba9d83a138f33b7
Cece Shalevska <cshalevska@gmail.com>
2022-03-10 23:02:41 -0500

    all good

--- class21/SuperTester.java
+++
@@ -1,8 +0,0 @@
-package class21;
-    public class SuperTester {
-        public static void main(String[] args) {
-            Child child=new Child();
-            child.printColor();
-            child.print();
-        }
-    }
